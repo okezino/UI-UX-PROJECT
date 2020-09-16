0c073343e90180a1b84f07df8cc0aa269dec2f74
okezino <okezi002@gmail.com>
2020-09-16 23:10:48 +0100

    Updates

---
+++ precache-manifest.d0509adcdb0666b79ebc31d361f7ce70.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b3e3543ee7e4b5efa089cbba957d7d",
+    "url": "/UI-UX-PROJECT/index.html"
+  },
+  {
+    "revision": "a0d3c5f6d3e60b3ff9e7",
+    "url": "/UI-UX-PROJECT/static/css/main.2f3fe2df.chunk.css"
+  },
+  {
+    "revision": "e43c5412d56e46f7fd08",
+    "url": "/UI-UX-PROJECT/static/js/2.48fb600e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/UI-UX-PROJECT/static/js/2.48fb600e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d3c5f6d3e60b3ff9e7",
+    "url": "/UI-UX-PROJECT/static/js/main.9bd2f1ba.chunk.js"
+  },
+  {
+    "revision": "b7b51d2e453a8c083a96",
+    "url": "/UI-UX-PROJECT/static/js/runtime-main.b830e528.js"
+  },
+  {
+    "revision": "69d48b1d6647bec197e8c6da5467e899",
+    "url": "/UI-UX-PROJECT/static/media/Bg.69d48b1d.png"
+  },
+  {
+    "revision": "c22eaa670dffe2ce5c33eddfaa889cfe",
+    "url": "/UI-UX-PROJECT/static/media/home.c22eaa67.png"
+  },
+  {
+    "revision": "f22e8719900fd42aa05b2a5b32a13b25",
+    "url": "/UI-UX-PROJECT/static/media/icon _fb.f22e8719.png"
+  },
+  {
+    "revision": "c4a6241f38dbe1d6790c012da22d2a93",
+    "url": "/UI-UX-PROJECT/static/media/icon _google.c4a6241f.png"
+  },
+  {
+    "revision": "27bd2c32c52d36c2a624dca0d3089415",
+    "url": "/UI-UX-PROJECT/static/media/icon _mail.27bd2c32.png"
+  },
+  {
+    "revision": "e5d8f8e749474524299b60e8c839b013",
+    "url": "/UI-UX-PROJECT/static/media/object2.e5d8f8e7.png"
+  },
+  {
+    "revision": "6e0f7aea6b805e7a20ca93c6f6e37e41",
+    "url": "/UI-UX-PROJECT/static/media/object3.6e0f7aea.png"
+  },
+  {
+    "revision": "f5d86cff0f277303a3798629bec7262e",
+    "url": "/UI-UX-PROJECT/static/media/ph.f5d86cff.png"
+  }
+]);
